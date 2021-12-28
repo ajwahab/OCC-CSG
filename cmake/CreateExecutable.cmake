d283b8d143533173416150549f6fdd0aa5e2d64f
Adam Wahab <adam.wahab@gmail.com>
2021-12-28 11:52:06 -0500

    cleaned up cmakelists

--- cmake/CreateExecutable.cmake
+++
@@ -1,9 +0,0 @@
-function(CreateExecutable EXECUTABLESOURCE)
-	
-	get_filename_component(EXECUTABLEBINARY ${EXECUTABLESOURCE} NAME_WE)
-
-	add_executable(${EXECUTABLEBINARY} ${EXECUTABLESOURCE})
-	target_link_libraries(${EXECUTABLEBINARY} ${ARGN})
-
-
-endfunction(CreateExecutable)
--- cmake/CreateLibrary.cmake
+++
@@ -1,9 +0,0 @@
-macro(CreateLibrary)
-	
-	file(GLOB HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/inc/*.hpp")
-	file(GLOB SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp")
-
-	add_library(${PROJECT_NAME} ${SOURCES} ${HEADERS})
-	target_link_libraries(${PROJECT_NAME} ${ARGN})
-
-endmacro(CreateLibrary)
